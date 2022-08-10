02514d29432a3929f3faa6eeebc1fb78a0e86552
Simrun <simrun.mutha@gmail.com>
2022-08-10 09:54:05 -0400

    Add confluence method

--- src/main.py
+++ src/main.py
@@ -5,41 +5,46 @@ from os.path import join
 import requests
 from markdown import markdown
 from mdx_gfm import GithubFlavoredMarkdownExtension
-from atlassian import confluence
+from atlassian import Confluence
 
 workspace = environ.get('GITHUB_WORKSPACE')
 if not workspace:
     raise Exception('No workspace is set')
 
+
 envs: Dict[str, str] = {}
-for key in ['from', 'to', 'cloud', 'user', 'token']:
+for key in ['from', 'parent_id', 'cloud', 'user', 'token']:
     value = environ.get(f'INPUT_{key.upper()}')
     if not value:
         raise Exception(f'Missing value for {key}')
     envs[key] = value
 
+
+confluence = Confluence(url="http://localhost:8090", username="simrun", password=envs['token'])
+
 with open(join(workspace, envs['from'])) as f:
     md = f.read()
 
-url = f"https://{envs['cloud']}.atlassian.net/wiki/rest/api/content/{envs['to']}"
+# url = f"https://{envs['cloud']}.atlassian.net/wiki/rest/api/content/{envs['to']}"
 
-current = requests.get(url, auth=(envs['user'], envs['token'])).json()
+# current = requests.get(url, auth=(envs['user'], envs['token'])).json()
 
 html = markdown(md, extensions=[GithubFlavoredMarkdownExtension()])
-content = {
-    'id': current['id'],
-    'type': current['type'],
-    'title': current['title'],
-    'version': {'number': current['version']['number'] + 1},
-    'body': {
-        'editor': {
-            'value': html,
-            'representation': 'editor'
-        }
-    }
-}
+# content = {
+#     'id': current['id'],
+#     'type': current['type'],
+#     'title': current['title'],
+#     'version': {'number': current['version']['number'] + 1},
+#     'body': {
+#         'editor': {
+#             'value': html,
+#             'representation': 'editor'
+#         }
+#     }
+# }
+confluence.create_page("simrun", "Release Notes", html, parent_id=envs['parent_id'], type='page', representation='storage', editor='v2')
 
-updated = requests.put(url, json=content, auth=(
-    envs['user'], envs['token'])).json()
-link = updated['_links']['base'] + updated['_links']['webui']
-print(f'Uploaded content successfully to page {link}')
+# updated = requests.put(url, json=content, auth=(
+#     envs['user'], envs['token'])).json()
+# link = updated['_links']['base'] + updated['_links']['webui']
+# print(f'Uploaded content successfully to page {link}')
