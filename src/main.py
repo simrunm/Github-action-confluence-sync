dfde28953ab4e232759e5fbcc6a6f4556b9608f8
Simrun <simrun.mutha@gmail.com>
2022-08-10 11:01:00 -0400

    Remove type is page

--- src/main.py
+++ src/main.py
@@ -48,7 +48,7 @@ html = markdown(md, extensions=[GithubFlavoredMarkdownExtension()])
 #         }
 #     }
 # }
-confluence.create_page(space= "Engineering", title="Release Notes", body=html, parent_id=envs['parent_id'], type='page')
+confluence.create_page(space= "Engineering", title="Release Notes", body=html, parent_id=envs['parent_id'])
 
 # updated = requests.put(url, json=content, auth=(
 #     envs['user'], envs['token'])).json()
